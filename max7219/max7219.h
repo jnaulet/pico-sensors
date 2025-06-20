fc1fb641b4f2165dfda0878911eeac61b9f47324
Joachim Naulet <joachim.naulet.ext@maia-space.com>
2025-06-20 14:07:33 +0200

    max7219: support for 8 segments chip

---
+++ max7219/max7219.h
@@ -0,0 +1,44 @@
+#ifndef MAX7219_H
+#define MAX7219_H
+
+#include <stdint.h>
+#include <gpio.h>
+#include <spi.h>
+
+typedef enum {
+    MAX7219_ADDR_NOOP           = 0x0,
+    MAX7219_ADDR_DIGIT0         = 0x1,
+    MAX7219_ADDR_DIGIT1         = 0x2,
+    MAX7219_ADDR_DIGIT2         = 0x3,
+    MAX7219_ADDR_DIGIT3         = 0x4,
+    MAX7219_ADDR_DIGIT4         = 0x5,
+    MAX7219_ADDR_DIGIT5         = 0x6,
+    MAX7219_ADDR_DIGIT6         = 0x7,
+    MAX7219_ADDR_DIGIT7         = 0x8,
+    MAX7219_ADDR_DECODE_MODE    = 0x9,
+    MAX7219_ADDR_INTENSITY      = 0xa,
+    MAX7219_ADDR_SCAN_LIMIT     = 0xb,
+    MAX7219_ADDR_SHUTDOWN       = 0xc,
+    MAX7219_ADDR_DISPLAY_TEST   = 0xf,
+    MAX7219_ADDR_COUNT
+} max7219_addr_t;
+
+typedef enum {
+    MAX7219_STATE_SETUP,
+    MAX7219_STATE_XFER,
+    MAX7219_STATE_COUNT
+} max7219_state_t;
+
+struct max7219 {
+    /*@temp@*/ struct spi *spi;
+    /*@temp@*/ struct gpio *load;
+    max7219_state_t state;
+    int pos;
+};
+
+int max7219_init(/*@out@*/ struct max7219 *ctx, struct spi *spi, struct gpio *load);
+void max7219_release(struct max7219 *ctx);
+
+int max7219_send(struct max7219 *ctx, max7219_addr_t addr, const uint8_t *data, size_t n);
+
+#endif
